e4431002d8daf1a0391f69a0553f216ec3de4429
Mali Michael <malimccalla@gmail.com>
2016-09-24 12:06:20 +0100

    Pulls video data from YT API

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,9 +9,11 @@ class SearchBar extends Component {
 
   render() {
     return (
-    <div>
-      <input onChange={event => this.setState({term: event.target.value})}/>
-    </div>
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({term: event.target.value})}/>
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyDVLkAdyqYRzXrByOnTLN6KrBlj8y479zM';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      // this.setState({ videos: videos})
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
