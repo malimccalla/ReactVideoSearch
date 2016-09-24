72991e3b61c352619fa054d6a2e254f204e8d1fd
Mali Michael <malimccalla@gmail.com>
2016-09-24 14:26:08 +0100

    Video detail

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyDVLkAdyqYRzXrByOnTLN6KrBlj8y479zM';
 
 YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
@@ -25,6 +26,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos} />
       </div>
     );
