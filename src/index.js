4e569df61dc28d75bb3740e557068ad94a0177f6
Mali Michael <malimccalla@gmail.com>
2016-09-24 11:26:22 +0100

    SearchBar component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDVLkAdyqYRzXrByOnTLN6KrBlj8y479zM';
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
